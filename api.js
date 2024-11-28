4b001afd8490b12925b763f77079a70987bf2d9e
Coder Kid <rudranshsingh1110@gmail.com>
2024-11-28 21:41:07 +0530

    modified

--- api.js
+++ api.js
@@ -1,19 +1,14 @@
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the API');
-});
-
-app.get('/api', (req, res) => {
     res.json({
-        city: "Dwaraka",
+        city: "Dwarka",
         state: "Delhi",
         country: "India",
         slack_id: "U06DS214ULB",
-        extra: "im human"
+        extra: "Fun Fact about me : For breakfast, I take a bag of sugar, and a spoon. Then I start shoveling it into my mouth."
     });
 });
 
